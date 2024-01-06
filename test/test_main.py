2286e1aa6e542b49460ad007d55690739dcc6fd6
Jose Prieto <alberto.prieto.r@gmail.com>
2024-01-06 09:42:35 -0600

    test folder created
    Test folder added

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,8 @@
 import pytest
 from unittest.mock import patch, ANY
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import EnviaEmail, ClientError
 
 @pytest.fixture
